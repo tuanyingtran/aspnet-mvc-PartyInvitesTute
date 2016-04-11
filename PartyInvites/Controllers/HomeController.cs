5185576985b7a4af989e74647f25a31347fb906d
Tuan T <tuanyingchen@hotmail.com>
2016-04-10 21:33:43 -0700

    initial commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int currentHour = DateTime.Now.Hour;
+            string greeting = "Hello";
+            if (currentHour < 12) greeting = "Good Morning!";
+            else if (currentHour < 18) greeting = "Good Afternoon!";
+            else greeting = "Good Evening!";
+            ViewBag.Greeting = greeting;
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RspvForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RspvForm(GuestLogModel guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to party organizer
+                return View("Thanks", guestResponse);
+            } else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestLogModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestLogModel
+    {
+        [Required (ErrorMessage = "Please Enter First Name")]
+        public string FirstName { get; set; }
+
+        [Required (ErrorMessage ="Please Enter Last Name")]
+        public string LastName { get; set; }
+
+        [Required (ErrorMessage ="Please Enter Email")]
+        [RegularExpression (".+\\@.+\\..+", ErrorMessage ="Valid Email Needed!")]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage ="Please Enter Contact Number")]
+        [RegularExpression("\\(?\\d{3}\\)?-? *\\d{3}-? *-?\\d{4}", ErrorMessage ="Valid Phone Number Needed!")]
+        public string Phone { get; set; }
+
+        [Required (ErrorMessage ="Please Specify Weather You Will Attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
